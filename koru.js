b0f7f2076ee714b9dc4fb9cd3a7263f95fce926b
lsiminski <laurensiminski@gmail.com>
2016-04-27 11:59:52 -0400

    aadfgsdfg

--- koru.js
+++ koru.js
@@ -4,15 +4,16 @@ $.getJSON('koru.json', function(data) {
   var randomCard = data.promptcards[Math.floor(Math.random()*data.promptcards.length)];
   console.log(randomCard.card);
   $("#prompt p").text(randomCard.card);
-    
-});
-    
+     
+/*
   var randomCard = data.actioncards[Math.floor(Math.random()*data.actioncards.length)];
   console.log(randomCard.card);
   $("#action p").text(randomCard.card);
+*/
     
 });
 
+/*
 $('#prompt').click(function() {
   document.location.reload(true);
 });
@@ -20,3 +21,4 @@ $('#prompt').click(function() {
 $('#action').click(function() {
   document.location.reload(true);
 });
+*/
