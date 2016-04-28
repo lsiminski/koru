7bbebd75b186e51009c8970805d7e1986c1d67af
lsiminski <laurensiminski@gmail.com>
2016-04-28 16:40:44 -0400

    asdcasdc

--- koru.js
+++ koru.js
@@ -1,24 +1,19 @@
 
-$.getJSON('koru.json', function(data) { 
+$.getJSON('oblique.json', function(data) { 
     
-  var randomCard = data.promptcards[Math.floor(Math.random()*data.promptcards.length)];
-  console.log(randomCard.card);
-  $("#prompt p").text(randomCard.card);
-     
-/*
-  var randomCard = data.actioncards[Math.floor(Math.random()*data.actioncards.length)];
-  console.log(randomCard.card);
-  $("#action p").text(randomCard.card);
-*/
-    
+  var randomCard = data[Math.floor(Math.random()*data.length)];
+  console.log(randomCard);
+  /* $("#oblique_strategies p").text(randomStrategy.strategy); */
+
+$.each(data, function(data) {
+		  $("#oblique_category p").text(this.category);
+		  $("#oblique_prompt p").text(this.prompt);
+		  $("#oblique_icons p").html("<img src='" + this.url + "'>");
+		  $("body.random").css("background-color");
+  });
+
 });
 
-/*
-$('#prompt').click(function() {
+$('#oblique_prompt p').click(function() {
   document.location.reload(true);
 });
-
-$('#action').click(function() {
-  document.location.reload(true);
-});
-*/
